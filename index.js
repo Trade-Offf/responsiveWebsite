9cb92836dcc247df6a78d42789cfcc06bbf0c080
LiJialin <jialin.li@ceasia.com.cn>
2020-03-26 23:50:06 +0800

    add about-us

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const glide = new Glide(".glide");
+const captionsEL = document.querySelectorAll(".slide-caption");
 
+glide.on(["mount.after", "run.after"], () => {
+  const caption = captionsEL[glide.index];
+  anime({
+    targets: caption.children,
+    opacity: [0,1],
+    duration: 400,
+    easing: "linear",
+    delay: anime.stagger(400, { start: 300 }),
+    translateY: [anime.stagger([40,10]), 0]
+  });
+});
+
+glide.on("run.before", () => {
+  document.querySelectorAll(".slide-caption > *").forEach(el => {
+    el.style.opacity = 0;
+  })
+})
 glide.mount();

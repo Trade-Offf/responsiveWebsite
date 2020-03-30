48f1324753bd65a21dfdc538a49cce5d3f103af0
LiJialin <jialin.li@ceasia.com.cn>
2020-03-30 22:57:48 +0800

    add showcases

--- index.js
+++ index.js
@@ -5,17 +5,37 @@ glide.on(["mount.after", "run.after"], () => {
   const caption = captionsEL[glide.index];
   anime({
     targets: caption.children,
-    opacity: [0,1],
+    opacity: [0, 1],
     duration: 400,
     easing: "linear",
     delay: anime.stagger(400, { start: 300 }),
-    translateY: [anime.stagger([40,10]), 0]
+    translateY: [anime.stagger([40, 10]), 0]
   });
 });
 
 glide.on("run.before", () => {
   document.querySelectorAll(".slide-caption > *").forEach(el => {
     el.style.opacity = 0;
-  })
-})
+  });
+});
 glide.mount();
+
+const isotope = new Isotope(".cases", {
+  layoutMode: "fitRows",
+  itemSelector: ".case-item"
+});
+
+const filterBtns = document.querySelector(".filter-btns");
+
+filterBtns.addEventListener("click", e => {
+  let { target } = e;
+  const filterOption = target.getAttribute("data-filter");
+  if (filterOption) {
+    document
+      .querySelectorAll(".filter-btn.active")
+      .forEach(btn => btn.classList.remove("active"));
+    target.classList.add("active");
+
+    isotope.arrange({ filter: filterOption });
+  }
+});

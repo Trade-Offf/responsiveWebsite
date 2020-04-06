19cdad19034d6b3185797d5ca6c423c9824407c1
LiJialin <jialin.li@ceasia.com.cn>
2020-04-06 16:48:48 +0800

    add footer and animation

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+const headerEL = document.querySelector("header");
+const scrollToTop = document.querySelector(".scrollToTop");
+window.addEventListener("scroll", () => {
+  let height = headerEL.getBoundingClientRect().height;
+
+  if (window.pageYOffset - height > 800) {
+    if (!headerEL.classList.contains("sticky")) {
+      headerEL.classList.add("sticky");
+    }
+  } else {
+    headerEL.classList.remove("sticky");
+  }
+
+  if (window.pageYOffset > 2000) {
+    scrollToTop.style.display = "block";
+  } else {
+    scrollToTop.style.display = "none";
+  }
+});
+
 const glide = new Glide(".glide");
 const captionsEL = document.querySelectorAll(".slide-caption");
 
@@ -9,12 +29,12 @@ glide.on(["mount.after", "run.after"], () => {
     duration: 400,
     easing: "linear",
     delay: anime.stagger(400, { start: 300 }),
-    translateY: [anime.stagger([40, 10]), 0]
+    translateY: [anime.stagger([40, 10]), 0],
   });
 });
 
 glide.on("run.before", () => {
-  document.querySelectorAll(".slide-caption > *").forEach(el => {
+  document.querySelectorAll(".slide-caption > *").forEach((el) => {
     el.style.opacity = 0;
   });
 });
@@ -22,18 +42,18 @@ glide.mount();
 
 const isotope = new Isotope(".cases", {
   layoutMode: "fitRows",
-  itemSelector: ".case-item"
+  itemSelector: ".case-item",
 });
 
 const filterBtns = document.querySelector(".filter-btns");
 
-filterBtns.addEventListener("click", e => {
+filterBtns.addEventListener("click", (e) => {
   let { target } = e;
   const filterOption = target.getAttribute("data-filter");
   if (filterOption) {
     document
       .querySelectorAll(".filter-btn.active")
-      .forEach(btn => btn.classList.remove("active"));
+      .forEach((btn) => btn.classList.remove("active"));
     target.classList.add("active");
 
     isotope.arrange({ filter: filterOption });
